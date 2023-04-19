6164cef3c451b0568815defbd3a1f7edd16004f8
Jacob Bieker <jacob@bieker.tech>
2023-04-19 12:03:54 +0100

    Try fixing mask shape

--- pv_pseudo_experiments/irradiance/model.py
+++ pv_pseudo_experiments/irradiance/model.py
@@ -87,6 +87,7 @@ class LitIrradianceModel(LightningModule):
         y_hat = einops.repeat(y_hat, "b t h w -> b c t h w", c=1)
 
         mask = meta > 0.0
+        mask = torch.sum(mask, dim=1) > 0.0
 
         # Expand to match the ground truth shape
         mask = einops.repeat(mask, "b c h w -> b c t h w", t=y.shape[2])
@@ -117,6 +118,7 @@ class LitIrradianceModel(LightningModule):
         y_hat = einops.repeat(y_hat, "b t h w -> b c t h w", c=1)
 
         mask = meta > 0.0
+        mask = torch.sum(mask, dim=1) > 0.0
 
         # Expand to match the ground truth shape
         mask = einops.repeat(mask, "b c h w -> b c t h w", t=y.shape[2])
