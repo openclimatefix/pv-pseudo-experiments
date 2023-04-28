2b24e6075852e84f4d91915b54cf8da6369ef38d
Jacob Bieker <jacob@bieker.tech>
2023-04-28 14:13:33 +0100

    Update configs and batch writing

--- pv_pseudo_experiments/irradiance/batch_writing.py
+++ pv_pseudo_experiments/irradiance/batch_writing.py
@@ -2,13 +2,14 @@ import datetime
 import torch
 from ocf_datapipes.training.pseudo_irradience import pseudo_irradiance_datapipe
 from torch.utils.data.dataloader import DataLoader
+import os
 
 class BatchWriter():
     def __init__(self, dataloader_config):
         datapipe = pseudo_irradiance_datapipe(
             dataloader_config.config,
             start_time=datetime.datetime(2008, 1, 1),
-            end_time=datetime.datetime(2020, 12, 31),
+            end_time=datetime.datetime(2021, 12, 31),
             use_sun=dataloader_config.sun,
             use_nwp=dataloader_config.nwp,
             use_sat=dataloader_config.sat,
@@ -25,6 +26,9 @@ class BatchWriter():
                   num_workers=dataloader_config.num_workers, batch_size=None)
 
     def __call__(self):
+        count = 0
         for i, batch in enumerate(self.dataloader):
-            batch = (torch.Tensor(batch[0]), torch.Tensor(batch[1]), torch.Tensor(batch[2]))
-            torch.save(batch, f"/mnt/disks/batches/{i}.pth")
+            batch = (torch.stack(batch[0], dim=0), torch.stack(batch[1], dim=0), torch.stack(batch[2], dim=0))
+            while os.path.exists(f"/mnt/storage_ssd_4tb/irradiance_batches/sun/2021_{count}.pth"):
+                count += 1
+            torch.save(batch, f"/mnt/storage_ssd_4tb/irradiance_batches/sun/2021_{count}.pth")
